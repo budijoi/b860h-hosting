344eaabe7c3d813180cf07b789529f37c0a47136
MOCHVPN <91376150+budijoi@users.noreply.github.com>
2026-06-06 15:29:07 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,68 @@
 #!/bin/bash
 
-curl -fsSL \
-https://raw.githubusercontent.com/budijoi/Script-Installer-Web-Hosting-B860H/refs/heads/main/sc-v1.sh \
--o /tmp/install-hosting.sh
+# ==================================================
+# BUDIJOI SERVER INSTALLER
+# Version : 1.0
+# ==================================================
 
-chmod +x /tmp/install-hosting.sh
+clear
 
-sudo /tmp/install-hosting.sh
+INSTALL_INFO="/root/budijoi-server-info.txt"
+
+echo "========================================="
+echo "      BUDIJOI SERVER INSTALLER"
+echo "========================================="
+echo ""
+
+# Root check
+if [ "$EUID" -ne 0 ]; then
+    echo "[ERROR] Jalankan sebagai root!"
+    exit 1
+fi
+
+# Detect system information
+
+HOSTNAME=$(hostname)
+KERNEL=$(uname -r)
+IP=$(hostname -I | awk '{print $1}')
+CPU=$(grep "model name" /proc/cpuinfo | head -n1 | cut -d ":" -f2 | xargs)
+
+MEMORY=$(free -h | awk '/Mem:/ {print $2}')
+
+echo "Hostname : $HOSTNAME"
+echo "Kernel   : $KERNEL"
+echo "IP       : $IP"
+echo "RAM      : $MEMORY"
+echo ""
+
+echo "========================================="
+echo " Tahapan Instalasi"
+echo "========================================="
+echo "1. System Preparation"
+echo "2. Storage Detection"
+echo "3. Swap Configuration"
+echo "4. Install NGINX"
+echo "5. Install PHP-FPM"
+echo "6. Install MariaDB"
+echo "7. Install File Browser"
+echo "8. Configure Firewall"
+echo "9. Generate Web Pages"
+echo "10. Finish"
+echo ""
+
+read -p "Tekan ENTER untuk memulai..."
+
+mkdir -p modules
+
+echo "========================================="
+echo "Memulai Instalasi..."
+echo "========================================="
+
+# Jalankan Module
+
+bash modules/01-system.sh
+bash modules/02-storage.sh
+
+echo ""
+echo "Tahap berikutnya akan dibuat pada modul selanjutnya."
+echo ""
